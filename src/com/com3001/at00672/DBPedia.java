1879305302a0c37f9452c0db39006f71978c0652
Alex Turner <alexwilliamturner@gmail.com>
2019-11-13 12:48:59 +0000

    Completed slide deck

--- src/com/com3001/at00672/DBPedia.java
+++ src/com/com3001/at00672/DBPedia.java
@@ -1,4 +1,34 @@
 package com.com3001.at00672;
 
+import org.apache.jena.query.ParameterizedSparqlString;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.ResourceFactory;
+
 public class DBPedia {
+    public static void main(String[] args) {
+        ParameterizedSparqlString qs = new ParameterizedSparqlString(""
+                + "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
+                + "PREFIX dbo:     <http://dbpedia.org/ontology/>"
+                + "\n"
+                + "select distinct ?resource ?abstract where {\n"
+                + "  ?resource rdfs:label 'Java (programming language)'@en.\n"
+                + "  ?resource dbo:abstract ?abstract.\n"
+                + "  FILTER (lang(?abstract) = 'en')}");
+
+
+        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
+
+        ResultSet results = exec.execSelect();
+
+        while (results.hasNext()) {
+
+            System.out.println(results.next().get("abstract").toString());
+        }
+
+        ResultSetFormatter.out(results);
+    }
 }

ba6f8878a7e560d577060efaf636062ae0ff321c
Alex <alexwilliamturner@gmail.com>
2020-02-19 18:53:45 +0000

    Initialise python/django project

---
+++ pydiaBot/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alex\Uni\COM3001\com3001_at00672\pydiaBot\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- wikibot/src/main/java/com3001.at00672/ChatbotUtils.java
+++ wikibot/src/main/java/com3001.at00672/ChatbotUtils.java
@@ -2,40 +2,37 @@ package com3001.at00672;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.log4j.varia.NullAppender;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class ChatbotUtils {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Loading countries set");
-        String dir = System.getProperty("user.dir");
-        String countriesPath = dir + "/src/main/resources/bots/wikibot/sets/countries.txt";
-        System.out.println(countriesPath);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(countriesPath));
-        // setup connection
-        String countriesQuery = " PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
-                " PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
-                " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                " PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
-                " SELECT ?place, ?label WHERE {\n" +
-                " ?place a dbo:Country .\n" +
-                " ?place rdfs:label ?label .\n" +
-                " FILTER langMatches(lang(?label), 'en')}" +
-                " ORDER BY ASC(?label) LIMIT 100 ";
-        Query query = QueryFactory.create(countriesQuery);
-        QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
-        ResultSet results = qexec.execSelect();
-        // TODO: loop through all results
-        ArrayList<String> resultsList = new ArrayList();
-        while (results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-            System.out.println(solution.toString());
-            writer.write(solution.toString());
+    public static void main(String[] args) {
+        loadPersonSubclasses();
+    }
+
+    public static void loadPersonSubclasses() {
+        try {
+            org.apache.log4j.BasicConfigurator.configure();
+            String queryString = " PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+                    " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                    " PREFIX yago: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                    "  SELECT distinct ?type WHERE {\n" +
+                    " ?class rdfs:subClassOf* dbo:Person .\n" +
+                    " ?class rdfs:label ?label\n" +
+                    " FILTER langMatches(lang(?label), 'en') ." +
+                    " BIND (STR(?label)  AS ?type)." +
+                    " }";
+            Query query = QueryFactory.create(queryString);
+            QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+            ResultSet results = qexec.execSelect();
+            // TODO: loop through all results
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                System.out.println(soln.getLiteral("type"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        writer.close();
     }
 }

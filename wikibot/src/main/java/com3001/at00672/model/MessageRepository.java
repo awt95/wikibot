131b9771cf8bc56383170a575f6a48e91f4405b3
Alex Turner <alexwilliamturner@gmail.com>
2020-05-07 13:29:35 +0100

    Scope ChatbotService to session to allow multiple users

--- wikibot/src/main/java/com3001/at00672/model/MessageRepository.java
+++ wikibot/src/main/java/com3001/at00672/model/MessageRepository.java
@@ -3,5 +3,9 @@ package com3001.at00672.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MessageRepository extends CrudRepository<Message, Long> {}
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    public List<Message> findBySessionEquals(String sessionId);
+}
